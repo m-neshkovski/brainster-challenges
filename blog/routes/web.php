23f7555af2e3d5ac7c53b8a764758a849fe0ef0f
Milosh Neshkovski pc-work <m.neshkovski@gmail.com>
2021-02-17 15:31:48 +0100

    resena zadaca

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\RoutingController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RoutingController::class, 'home'])->name('home');
+Route::get('/about', [RoutingController::class, 'about'])->name('about');
+Route::get('/post', [RoutingController::class, 'post'])->name('post');
+Route::get('/contact', [RoutingController::class, 'contact'])->name('contact');
