91d89aa4458dc472805215b6e12f418540f0764d
Milosh Neshkovski pc-work <m.neshkovski@gmail.com>
2021-03-18 16:57:56 +0100

    commit na mail.php

--- laravelDB/config/mail.php
+++ laravelDB/config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => 'mail.neshkovski.com',
-            'port' => 465,
-            'encryption' => 'ssl',
-            'username' => 'test@neshkovski.com',
-            'password' => 'Kumanovo123.',
+            'host' => env('MAIL_HOST', 'mail.neshkovski.com'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'username' => env('MAIL_USERNAME', 'test@neshkovski.com'),
+            'password' => env('MAIL_PASSWORD', 'Kumanovo123.'),
             'timeout' => null,
             'auth_mode' => null,
         ],
